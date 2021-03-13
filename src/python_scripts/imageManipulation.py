fixed issue with cropping and scaling digits properly for recognition

src/python_scripts/imageManipulation.py
@@ -16,72 +16,72 @@ save_path = str(pathlib.Path.joinpath(path,  'image_recognition'))
 
 cap = cv2.VideoCapture(full_path)
 i = 0
+img_quality = 100
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    cv2.imwrite(save_path + '/frame.jpeg', frame)
-    i += 1
+# Starting point for cropping digits is the 10's digit position:
+w = 11
+h = 12
+x = 3
+y = 125
 
-cap.release()
-cv2.destroyAllWindows()
-
-# For the 10's digit:
-
-w = 12
-h = 20
-x = 2
-y = 120
-
-ones_x_offset = x + 11
-decimal_x_offset = x + 29
-
-img = cv2.imread(save_path + '/frame.jpeg')
-
-roi_tens = img[y:y+h, x:x+w]
-roi_ones = img[y:y+h, ones_x_offset : ones_x_offset + w]
-roi_decimal = img[y:y+h, decimal_x_offset : decimal_x_offset + w]
-
-# our model is expecting the images to be 28 x 28
-y_padding = int((28 - h) / 2)
-x_padding = int((28 - w) / 2)
-
-roi_tens = cv2.copyMakeBorder(
-    roi_tens, 
-    y_padding, 
-    y_padding, 
-    x_padding, 
-    x_padding, 
-    cv2.BORDER_CONSTANT)
-
-# hand-tune this one because the wasn't centered enough
-roi_ones = cv2.copyMakeBorder(
-    roi_ones,
-    y_padding,
-    y_padding,
-    x_padding - 1,
-    x_padding + 1,
-    cv2.BORDER_CONSTANT)
-
-# hand-tune this one because the wasn't centered enough
-roi_decimal = cv2.copyMakeBorder(
-    roi_decimal,
-    y_padding,
-    y_padding,
-    x_padding - 2, 
-    x_padding + 2,
-    cv2.BORDER_CONSTANT)
+ones_x_offset = x + 12
+decimal_x_offset = x + 31
 
 roi_tens_file_path = save_path + '/roi_tens.jpeg'
 roi_ones_file_path = save_path + '/roi_ones.jpeg'
 roi_decimal_file_path = save_path + '/roi_decimal.jpeg'
 
-cv2.imwrite(roi_tens_file_path, roi_tens)
-cv2.imwrite(roi_ones_file_path, roi_ones)
-cv2.imwrite(roi_decimal_file_path, roi_decimal)
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    
+    cv2.imwrite(roi_tens_file_path, frame[y:y+h, x:x+w],)
+    cv2.imwrite(roi_ones_file_path, frame[y:y+h, ones_x_offset: ones_x_offset + w],)
+    cv2.imwrite(roi_decimal_file_path, frame[y:y+h, decimal_x_offset: decimal_x_offset + w],)
+    
+    i += 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+roi_tens = cv2.imread(save_path + '/roi_tens.jpeg')
+roi_ones = cv2.imread(save_path + '/roi_ones.jpeg')
+roi_decimal = cv2.imread(save_path + '/roi_decimal.jpeg')
+
+imgs = [roi_tens, roi_ones, roi_decimal]
+
+# image manipulation
+# first, we scale the crop to 20 px then we pad to 28 (as per mnist)
+scale_pert = round(20/h, 3)
+# print(scale_pert)
+
+width = int(11 * scale_pert)
+height = int(12 * scale_pert)
+
+dim = (width, height)
+
+# our model is expecting the images to be 28 x 28
+x_padding = int((28 - width) / 2)
+y_padding = int((28 - height) / 2)
 
 img_file_paths = [roi_tens_file_path, roi_ones_file_path, roi_decimal_file_path]
+
+for i, image in enumerate(imgs):
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    # roi_ones = cv2.resize(roi_ones, dim, interpolation=cv2.INTER_AREA)
+    # roi_decimal = cv2.resize(roi_decimal, dim, interpolation=cv2.INTER_AREA)
+
+    image = cv2.copyMakeBorder(
+        image, 
+        y_padding, 
+        y_padding, 
+        x_padding, 
+        x_padding, 
+        cv2.BORDER_CONSTANT)
+
+    cv2.imwrite(img_file_paths[i], image)
+
 prediction_list = []
 
 model = tf.keras.models.load_model(save_path + '/saved_model/mnist_model')
@@ -95,7 +95,7 @@ def imgs_to_pred(file_names):
     for i in range(len(img_file_paths)):
         # get images to a form that tensorflow wants
         img = tf.io.read_file(img_file_paths[i])
-        img = tf.image.decode_png(img, channels=1)
+        img = tf.image.decode_jpeg(img, channels=1)
         img = img / 255
 
         img = tf.reshape(img, [1, 28, 28])